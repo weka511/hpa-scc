2932188d26e40cea054c6393b8feb030ca339551
Simon Crase <simon@greenweaves.nz>
2021-01-31 10:45:47 +1300

    #1 Read train.csv and parse command line parameters

--- visualize.py
+++ visualize.py
@@ -16,30 +16,81 @@
 #  To contact me, Simon Crase, email simon@greenweaves.nz
 
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+from matplotlib.image import imread
 import os
 import numpy as np
+import argparse
+
+Descriptions = [
+    'Nucleoplasm',
+    'Nuclear membrane',
+    'Nucleoli',
+    'Nucleoli fibrillar center',
+    'Nuclear speckles',
+    'Nuclear bodies',
+    'Endoplasmic reticulum',
+    'Golgi apparatus',
+    'Intermediate filaments',
+    'Actin filaments',
+    'Microtubules',
+    'Mitotic spindle',
+    'Centrosome',
+    'Plasma membrane',
+    'Mitochondria',
+    'Aggresome',
+    'Cytosol',
+    'Vesicles and punctate cytosolic patterns',
+    'Negative'
+]
 
 colours     = ['red',  'green', 'blue', 'yellow']
-colour_maps = [plt.cm.Reds,plt.cm.Blues,plt.cm.Greens,plt.cm.Oranges]
-meanings    = ['Microtubules', 'Nucleus', 'Protein/antibody', 'Endoplasmic reticulum']
-fig, axs    = plt.subplots(2, 2)
-path        = r'C:\data\hpa-scc\train512512'
+meanings    = ['Microtubules', 'Nuclei channels', 'Protein/antibody', 'Endoplasmic reticulum channels']
 image_id    = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0'
-image       = None
 
-for i in range(2):
-    for j in range(2):
-        index      = 2*i+j
-        file_name  = f'{image_id}_{colours[index]}.png'
-        path_name  = os.path.join(path,file_name)
-        image_mono = mpimg.imread(path_name)
-        if index==0:
-            nx,ny   = image_mono.shape
-            image   = np.zeros((nx,ny,4))
-        image[:,:,index] = image_mono
- 
-axs[0,0].imshow(image[:,:,[0,2,3]])
-        
-fig.suptitle(image_id)
-plt.show()
+
+def read_training_expectations(path=r'C:\data\hpa-scc',file_name='train.csv'):
+    header    = True
+    Training  = {}
+    for line in open(os.path.join(path,file_name)):
+        if header:
+            header = False
+            continue
+        trimmed = line.strip().split(',')
+        Training[trimmed[0]] =  [int(l) for l in trimmed[1].split('|')]
+    return Training
+
+def read_image(path        = r'C:\data\hpa-scc',
+               image_set   = 'train512512',
+               image_id    = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0'):
+    Image = None
+    for i in range(2):
+        for j in range(2):
+            index      = 2*i+j
+            file_name  = f'{image_id}_{colours[index]}.png'
+            path_name  = os.path.join(path,image_set,file_name)
+            image_mono = imread(path_name)
+            if index==0:
+                nx,ny   = image_mono.shape
+                Image   = np.zeros((nx,ny,4))
+            Image[:,:,index] = image_mono
+    return Image        
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser('Visualize HPA data')
+    parser.add_argument('--path',      default=r'C:\data\hpa-scc')
+    parser.add_argument('--image_set', default = 'train512512')
+    parser.add_argument('--image_id',  default = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0')
+    args     = parser.parse_args()
+    
+    Training = read_training_expectations(path=args.path)
+    Image    = read_image(path=args.path,image_id=args.image_id,image_set=args.image_set)
+    
+    fig = plt.figure(figsize=(20,20))
+    axs = fig.subplots(2, 2)
+    
+    axs[0,0].imshow(Image[:,:,[0,2,3]])
+    axs[0,0].axes.xaxis.set_ticks([])
+    axs[0,0].axes.yaxis.set_ticks([])
+    mylabels =  '+'.join([Descriptions[label] for label in Training[image_id]])      
+    fig.suptitle(f'{args.image_id}: {mylabels}')
+    plt.show()
