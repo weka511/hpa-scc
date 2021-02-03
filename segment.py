1a228601d75d118d483838dd581aa8430b14772d
Simon Crase <simon@greenweaves.nz>
2021-02-03 13:06:10 +1300

    #2 test with all files

--- segment.py
+++ segment.py
@@ -14,11 +14,12 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 #  To contact me, Simon Crase, email simon@greenweaves.nz
+#
+# Segment images using Otsu's method
 
 from   argparse          import ArgumentParser
-from   matplotlib.pyplot import figure, show
+from   matplotlib.pyplot import figure, show, cm, close
 from   matplotlib.image  import imread
-from   matplotlib        import cm
 from   numpy             import zeros, array, var, mean, std
 from   os                import remove
 from   os.path           import join,basename
@@ -99,44 +100,6 @@ def create_selection(Image,
   
     return Product
 
-def get8components(Image,threshold=0.5,nx=512,ny=512,deltas=[-1,0,1]):
-    def find_first_ripe():
-        for i in range(nx):
-            for j in range(ny):
-                if Open[i,j] and not Closed[i,j]:
-                    return i,j
-    
-                       
-    Closed   = zeros((nx,ny),dtype=bool)
-    Open     = zeros((nx,ny),dtype=bool)
-    Counts   = []
-    Component = []
-    for i in range(nx):
-        for j in range(ny):
-            if Image[i,j,BLUE]>threshold:
-                Open[i,j] = True
-    Ripe = set([find_first_ripe()])
-    while True:
-        while len(Ripe)>0:
-            i,j         = Ripe.pop()
-            Closed[i,j] = True
-            Component.append((i,j))
-            for delta_i in deltas:
-                for delta_j in deltas:
-                    if delta_i==0 and delta_j==0: continue
-                    i1 = i+delta_i
-                    if i1<0 or i1>=nx: continue
-                    j1 = j+delta_j
-                    if j1<0 or j1>=ny: continue
-                    if Image[i1,j1,BLUE]>threshold and not Closed[i1,j1] and not (i1,j1) in Ripe:
-                        Ripe.add((i1,j1))
-        yield Component
-        
-        next_set = find_first_ripe()
-        if next_set == None: return
-        Ripe    = set([next_set])               
-        Component = []
-
 def otsu(Image,nx,ny,tolerance=0.0001,N=50,ax1=None,ax2=None):
     def get_icv(threshold):
         P1   = [blue for blue in Blues if blue<threshold]
@@ -179,16 +142,57 @@ def otsu(Image,nx,ny,tolerance=0.0001,N=50,ax1=None,ax2=None):
     
     return threshold_mid,icv_mid
 
+
+def generate8components(Image,threshold=0.5,nx=512,ny=512,deltas=[-1,0,1]):
+    def find_first_ripe():
+        for i in range(nx):
+            for j in range(ny):
+                if Open[i,j] and not Closed[i,j]:
+                    return i,j
+    
+    Open      = zeros((nx,ny),dtype=bool) # Set of points that are potentially in a component
+    
+    for i in range(nx):
+        for j in range(ny):
+            if Image[i,j,BLUE]>threshold:
+                Open[i,j] = True
+                
+    Closed    = zeros((nx,ny),dtype=bool)  # Set of points that have been processed already
+    
+    Ripe = set([find_first_ripe()])        # Here is where we start building a component
+    while True:
+        Component = []
+        while len(Ripe)>0:
+            i,j         = Ripe.pop()
+            Closed[i,j] = True
+            Component.append((i,j))
+            for delta_i in deltas:
+                for delta_j in deltas:
+                    if delta_i==0 and delta_j==0: continue     # ignore case where we don't move
+                    i1 = i+delta_i
+                    j1 = j+delta_j
+                    if i1<0 or i1>=nx or j1<0 or j1>=ny: continue   # Don't move outside image
+                    
+                    if Image[i1,j1,BLUE]>threshold and not Closed[i1,j1] and not (i1,j1) in Ripe:
+                        Ripe.add((i1,j1))
+        yield Component
+        
+        next_set = find_first_ripe()
+        if next_set == None: return
+        Ripe    = set([next_set])               
+
+
+
 def parse_tuple(s):
     return tuple([int(x) for x in s[1:-1].split(',')])
 
-def remove_false_findings(Image,threshold=-1,nx=256,ny=256):
+def remove_false_findings(Image,threshold=-1,nx=256,ny=256,axs=None):
     component_file = join(gettempdir(),f'{uuid4()}.txt')
     
     Areas = []
     
     with open(component_file,'w') as temp:
-        for Component in get8components(Image,threshold=threshold,nx=nx,ny=ny):
+        for Component in generate8components(Image,threshold=threshold,nx=nx,ny=ny):
             if len(Component)>1:
                 Areas.append(len(Component))
                 temp.write(' '.join([f'({x},{y})' for x,y in Component])  + '\n')
@@ -199,7 +203,7 @@ def remove_false_findings(Image,threshold=-1,nx=256,ny=256):
         print (a)
     P = mean(Areas)- std(Areas)
     print (f'{mean(Areas):0f} {std(Areas):0f}')
-    n,bins,_ = axs[1,2].hist(Areas,bins=25)    
+    n,bins,_ = axs.hist(Areas,bins=25)    
     
     Mask = zeros((nx,ny,3)) 
     
@@ -214,18 +218,8 @@ def remove_false_findings(Image,threshold=-1,nx=256,ny=256):
     
     return Mask
 
-if __name__=='__main__':
-    start  = time()
-    parser = ArgumentParser('Segment HPA data using Otsu\'s algorithm')
-    parser.add_argument('--path',                default=r'C:\data\hpa-scc')
-    parser.add_argument('--image_set',           default = 'train512512')
-    parser.add_argument('--image_id',            default = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0')
-    parser.add_argument('--show',                default=False, action='store_true')
- 
-    args     = parser.parse_args()
-    
-    Training = read_training_expectations(path=args.path)
-    Image    = read_image(path=args.path,image_id=args.image_id,image_set=args.image_set)
+def segment(args,image_id):    
+    Image    = read_image(path=args.path,image_id=image_id,image_set=args.image_set)
     nx,ny,_  = Image.shape
     
     fig      = figure(figsize=(20,20))
@@ -234,25 +228,49 @@ if __name__=='__main__':
     im       = axs[0,0].imshow(Image[:,:,BLUE],cmap=cm.get_cmap('Blues'))
     axs[0,0].axes.xaxis.set_ticks([])
     axs[0,0].axes.yaxis.set_ticks([]) 
-    axs[0,0].set_title(args.image_id)
+    axs[0,0].set_title(image_id)
     fig.colorbar(im, ax=axs[0,0], orientation='vertical')
     
-    threshold,icv = otsu(Image,nx,ny,ax1=axs[0,1],ax2=axs[1,0])
+    threshold,icv = otsu(Image,nx,ny,ax1=axs[1,1],ax2=axs[1,0])
     Partitioned   = zeros((nx,ny,3))
     for i in range(nx):
         for j in range(ny):
             if Image[i,j,BLUE]>threshold:
                 Partitioned[i,j,BLUE] = Image[i,j,BLUE]
-    axs[1,1].imshow(Partitioned) 
-    axs[1,1].axes.xaxis.set_ticks([])
-    axs[1,1].axes.yaxis.set_ticks([]) 
-    axs[1,1].set_title('Partitioned')
+                
+    axs[0,1].imshow(Partitioned) 
+    axs[0,1].axes.xaxis.set_ticks([])
+    axs[0,1].axes.yaxis.set_ticks([]) 
+    axs[0,1].set_title('Partitioned')
     
-    axs[0,2].imshow(remove_false_findings(Image,threshold=threshold,nx=nx,ny=ny))
+    axs[0,2].imshow(remove_false_findings(Image,threshold=threshold,nx=nx,ny=ny,axs=axs[1,2]))
     
     fig.suptitle(f'{"+".join([Descriptions[label] for label in Training[image_id]])  }')
 
-    fig.savefig(f'{basename(__file__).split(".")[0]}.png')
+    fig.savefig(join(args.figs,f'{image_id}.png'))
+    
+    if not args.show:
+        close(fig)
+    
+if __name__=='__main__':
+    start  = time()
+    parser = ArgumentParser('Segment HPA data using Otsu\'s algorithm')
+    parser.add_argument('--path',                default=r'C:\data\hpa-scc')
+    parser.add_argument('--image_set',           default = 'train512x512')
+    parser.add_argument('--image_id',            default = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0')
+    parser.add_argument('--show',                default=False, action='store_true')
+    parser.add_argument('--figs',                default= './figs')
+    parser.add_argument('--all',                 default=False, action='store_true')
+    args     = parser.parse_args()
+    
+    Training = read_training_expectations(path=args.path)
+    if args.all:
+        for image_id in sorted(Training.keys()):
+            print (image_id)
+            segment(args,image_id)    
+    else:
+        segment(args,args.image_id)
+
     
     elapsed = time() - start
     minutes = int(elapsed/60)
