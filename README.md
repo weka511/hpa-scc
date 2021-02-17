eb789e226806522e141c2d93659a94295e34ea57
Simon Crase <simon@greenweaves.nz>
2021-02-18 12:19:55 +1300

    #2 Started integrating HPA cell segmentor

--- README.md
+++ README.md
@@ -4,10 +4,12 @@
 ## Python Scripts
 
 |File|Description|
-|-----------------|-------------------------------------------------------------------------------------------------|
+|---------------------|-------------------------------------------------------------------------------------------------|
 ||Segmentation|
 |anayze-labels|Find images  that have only one label|
+|CellSegmenterTest.py|Demo for HPA cellsegmentator|
 |segment.py|Segment images using Otsu's method|
+|segment2.py|Segment images using HPA cellsegmentator|
 |watershed.py|Naive Watershed segmentation|
 ||Download and summarize data|
 |download.py|Download training images from kaggle|
---
+++ segment2.py
@@ -0,0 +1,73 @@
+#   Copyright (C) 2021 Greenweaves Software Limited
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU Lesser General Public
+#   License as published by the Free Software Foundation; either
+#   version 2.1 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU  Lesser General Public License for more details
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  To contact me, Simon Crase, email simon@greenweaves.nz
+#
+# Segment images using supplied CellSegmenter
+
+from hpacellseg.cellsegmentator  import CellSegmentator
+from hpacellseg.utils import label_cell, label_nuclei
+from argparse         import ArgumentParser
+from glob             import glob
+from os.path          import join
+from time             import time
+
+if __name__=='__main__':
+    start  = time()
+    parser = ArgumentParser('Segment HPA data using Otsu\'s algorithm')
+    parser.add_argument('--path',
+                        default = r'd:\data\hpa-scc',
+                        help    = 'Path to data')
+    parser.add_argument('--image_set',
+                        default = 'train512x512',
+                        help    = 'Identified subset of data-- e.g. train512x512')
+    parser.add_argument('--image_id',
+                        default = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0',
+                        help    = 'Identifies image to be segmented (if only one). See --sample, --all, and --read')
+    parser.add_argument('--show',
+                        default = False,
+                        action  = 'store_true',
+                        help    = 'Display plots')
+    parser.add_argument('--figs',
+                        default = './figs',
+                        help    = 'Identifies where to store plots')
+    parser.add_argument('--NuclearModel',
+                        default = './nuclei-model.pth')
+    parser.add_argument('--CellModel',
+                        default = './cell-model.pth')
+    args = parser.parse_args()
+
+    image_dir = join(args.path,args.image_set)
+    mt       = glob(join(image_dir, '*_red.png'))
+    er       = [f.replace('red', 'yellow') for f in mt]
+    nu       = [f.replace('red', 'blue') for f in mt]
+    images   = [mt, er, nu]
+
+    segmentator = CellSegmentator(
+        nuclei_model        = args.NuclearModel,
+        cell_model          = args.CellModel,
+        scale_factor        = 0.25,
+        device              = "cpu",
+        padding             = False,
+        multi_channel_model = True,
+    )
+
+    elapsed = time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+
+    if args.show:
+        show()
