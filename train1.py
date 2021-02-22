b0fc3b2c04dba145483757addfecb1e830df3ca6
Simon Crase <simon@greenweaves.nz>
2021-02-23 11:56:22 +1300

    #6 Coded up class for reading data

--- train1.py
+++ train1.py
@@ -16,36 +16,77 @@
 #  To contact me, Simon Crase, email simon@greenweaves.nz
 
 from argparse                   import ArgumentParser
+from matplotlib.pyplot          import figure, imread, imshow, axis, savefig, close, show, title,suptitle
+from numpy                      import array, clip
+from os.path                    import join
 from segment2                   import read_training_expectations
-from segment                    import read_image
+# from segment                    import read_image
+from skimage                    import io, transform
 from time                       import time
-from torch.utils.data           import Dataset
+from torch                      import from_numpy
 from torch.nn                   import Module, Conv2d, MaxPool2d, Linear,CrossEntropyLoss
 from torch.nn.functional        import relu
 from torch.optim                import SGD
-from torch                      import from_numpy
+from torch.utils.data           import Dataset, DataLoader
+from torchvision                import transforms, utils
 
+class CellDataset(Dataset):
+    def __init__(self,
+                 path         = r'd:\data\hpa-scc',
+                 image_set    = 'train512x512',
+                 file_name    = 'train.csv',
+                 transform    =  None):
+        self.expectations = read_training_expectations(path=path,file_name=file_name)
+        self.image_ids    = list(self.expectations.keys())
+        self.transform    = transform
+        self.path         = path
+        self.image_set    = image_set
 
+    def __len__(self):
+        return len(self.expectations)
 
+    def __getitem__(self, idx):
+        image_id        = self.image_ids[idx]
+        image_name      = f'{image_id}_red.png'
+        full_image_name = join(self.path,self.image_set,image_name)
+        img             = io.imread(full_image_name)
+        # sample          = {'image': img}
+
+        if self.transform:
+            sample = self.transform(img)
+
+        return img,self.expectations[image_id]
 
 class Net(Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = Conv2d(in_channels=4, out_channels=6, kernel_size=5,stride=1,padding=1)
+        self.conv1 = Conv2d(in_channels  = 3,
+                            out_channels = 6,
+                            kernel_size  = 5,
+                            stride       = 1,
+                            padding      = 1)
         self.pool  = MaxPool2d(2, 2)
-        self.conv2 = Conv2d(6, 16, 5)
-        self.fc1   = Linear(16 * 5 * 5, 120)
-        self.fc2   = Linear(120, 84)
-        self.fc3   = Linear(84, 10)
+        # self.conv2 = Conv2d(6, 16, 5)
+        # self.fc1   = Linear(16 * 5 * 5, 120)
+        # self.fc2   = Linear(120, 84)
+        # self.fc3   = Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(relu(self.conv1(x)))
-        x = self.pool(relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = relu(self.fc1(x))
-        x = relu(self.fc2(x))
-        x = self.fc3(x)
+        # x = self.pool(relu(self.conv2(x)))
+        # x = x.view(-1, 16 * 5 * 5)
+        # x = relu(self.fc1(x))
+        # x = relu(self.fc2(x))
+        # x = self.fc3(x)
         return x
+
+def imshow1(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    imshow(inp)
+    if title is not None:
+        title(title)
+
 
 if __name__=='__main__':
     start  = time()
@@ -60,19 +101,46 @@ if __name__=='__main__':
     parser.add_argument('--lr',       default = 0.001, type=float)
     args   = parser.parse_args()
 
+    training_data   = CellDataset()
+    training_loader = DataLoader(training_data,batch_size=4)
+    # Get a batch of training data
+    inputs, classes = next(iter(training_loader))
 
-    # training_set   = CellData()
-    # dataiter       = iter(training_set)
+    # Make a grid from batch
+    out = utils.make_grid(inputs)
 
-    net            = Net()
+    imshow1(out)
+
+    net             = Net()
     criterion = CrossEntropyLoss()
     optimizer = SGD(net.parameters(),
                           lr       = args.lr,
                           momentum = args.momentum)
-    image_id = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0'
-    image    = read_image(path=args.path,image_set=args.image_set,image_id=image_id)
-    net(from_numpy(image))
+
+    losses    = []
+
+    for epoch in range(1):#args.n):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(training_loader, 0):
+            inputs, labels = data # get the inputs; data is a list of [inputs, labels]
+            optimizer.zero_grad()  # zero the parameter gradients
+
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss    = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            running_loss += loss.item()
+            # if (i +1) % args.freq == 0:
+            print(f'{epoch + 1}, {i + 1}, {running_loss / args.freq}')
+            losses.append(running_loss/args.freq)
+            running_loss = 0.0
+
+
     elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
     print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+    show()
