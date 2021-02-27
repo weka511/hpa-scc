6e768c54289a4aafbe1753dbcd9efedbd2c455a8
Simon Crase <simon@greenweaves.nz>
2021-02-27 14:26:55 +1300

    #22 rolported latest validation code

--- train.py
+++ train.py
@@ -20,7 +20,7 @@
 from argparse                   import ArgumentParser
 from csv                        import reader
 from logs                       import get_logfile_names
-from numpy                      import asarray, stack, argmax
+from numpy                      import asarray, stack, argmax, mean
 from os.path                    import join, exists
 from os                         import walk
 from shutil                     import copy
@@ -108,20 +108,17 @@ class Net(Module):
         x = self.fc3(x)
         return softmax(x,dim=1)
 
-def imshow1(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    imshow(inp)
-    if title is not None:
-        title(title)
-
-def train(args):
+def get_logfile_name(args):
     logs          = get_logfile_names(False,None,args.prefix,args.suffix)
     i             = len(logs)
     logfile_path = join(args.logdir, f'{args.prefix}{i+1}{args.suffix}')
     while exists(logfile_path):
         i += 1
-        logfile_name = logfile_path = join(args.logdir, f'{args.prefix}{i+1}{args.suffix}')
+        logfile_path = join(args.logdir, f'{args.prefix}{i+1}{args.suffix}')
+    return logfile_path
+
+def train(args):
+    logs          = get_logfile_names(False,None,args.prefix,args.suffix)
 
     training_data   = CellDataset(file_name='training.csv')
     training_loader = DataLoader(training_data,batch_size=7)
@@ -159,17 +156,41 @@ def train(args):
                     save(model.state_dict(),save_weights_path )
                     running_loss = 0.0
 
+def get_index_file_name(index=None, default = 'validation.csv'):
+    return default  if index == None else index
+
+# get_predictions
+#
+# Use model to predict class of input
+def get_predictions(model,inputs):
+    def get_prediction(probabilities):
+        prediction = argmax(probabilities)
+        return prediction, probabilities[prediction]
+
+    output      = model(inputs)
+    predictions = [get_prediction(output[i].detach().numpy()) for i in range(len(output))]
+    return [a for a,_ in predictions],[b for _,b in predictions]
+
 def validate(args):
     model = Net()
     model.load_state_dict(load(f'{args.weights}.pth'))
     model.eval()
-    validation_data  = CellDataset(file_name = 'validation.csv')
-    validation_loader = DataLoader(validation_data,batch_size=1)
+    validation_loader = DataLoader(CellDataset(file_name =  get_index_file_name(index=args.index)),
+                                   batch_size=args.batch)
+    matches           = 0
+    mismatches        = 0
+    confidence        = 0.0
     for i, data in enumerate(validation_loader, 0):
         inputs, labels = data
-        predictions    = model(inputs)
-        j              = argmax(predictions[0].detach().numpy())
-        print (j,predictions[0].detach().numpy())
+        predictions,confidences = get_predictions(model,inputs)
+        confidence             += sum(confidences)
+        for predicted,expected in zip(predictions,[argmax(labels[i].detach().numpy()) for i in range(len(labels))]):
+            if predicted==expected:
+                matches+=1
+            else:
+                mismatches+=1
+        if (matches+mismatches)%args.freq==0:
+            print (f'Confidence = {100*confidence/(matches+mismatches):.1f}%, accuracy={100*matches/(matches+mismatches):.1f}%')
 
 
 def test(args):
@@ -216,6 +237,13 @@ if __name__=='__main__':
     parser.add_argument('--weights',
                         default = 'weights',
                         help    = 'Filename for saving and loading weights')
+    parser.add_argument('--index',
+                        default = None,
+                        help    = 'Name for index file for training/validation')
+    parser.add_argument('--batch',
+                        default = 7,
+                        type    = int,
+                        help    = 'Batch size for training/validating')
     args          = parser.parse_args()
 
     {   'train'    : train,
