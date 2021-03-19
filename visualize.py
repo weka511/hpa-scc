2fe17ee7d849b08dc54ef1a33c55d5ebac173144
Simon Crase <simon@greenweaves.nz>
2021-03-20 10:06:00 +1300

    #34 Allow visualization to be targetted to specifiec labels

--- visualize.py
+++ visualize.py
@@ -20,11 +20,10 @@ from csv               import reader
 from matplotlib.pyplot import figure, show, savefig, close
 from matplotlib.image  import imread
 from matplotlib        import cm
-from numpy             import zeros, array, mean, log
-from numpy.fft         import fft2
+from numpy             import zeros, array
 from os                import environ
 from os.path           import join
-from random            import sample
+from random            import sample, seed
 
 
 RED         = 0
@@ -68,49 +67,90 @@ def visualize(image_id     = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0',
               path         = join(environ['DATA'],'hpa-scc'),
               image_set    = 'train512x512',
               figs         = './figs',
-              Descriptions = []):
+              Descriptions = [],
+              dpi          = 300):
     fig          = figure(figsize=(20,20))
-    axs          = fig.subplots(2, 4)
+    axs          = fig.subplots(2, 2)
 
-    for column,colour in enumerate([BLUE,RED,YELLOW,GREEN]):
+    for seq,colour in enumerate([BLUE,RED,YELLOW,GREEN]):
         file_name        = f'{image_id}_{colours[colour]}.png'
         path_name        = join(path,image_set,file_name)
         grey_scale_image = imread(path_name)
         nx,ny            = grey_scale_image.shape
-
         Image            = zeros((nx,ny,NCOLOURS))
+
         if colour==YELLOW:
             Image[:,:,RED]    = grey_scale_image[:,:]
             Image[:,:,GREEN]  = grey_scale_image[:,:]
         else:
             Image[:,:,colour] = grey_scale_image[:,:]
 
-        axs[0,column].imshow(Image)
-        axs[0,column].axes.xaxis.set_ticks([])
-        axs[0,column].axes.yaxis.set_ticks([])
-        axs[0,column].set_title(meanings[colour])
-
-        freqs = fft2(grey_scale_image)
-        axs[1,column].imshow(log(abs(freqs)),cmap='gray')
+        axs[seq//2,seq%2].imshow(Image)
+        axs[seq//2,seq%2].axes.xaxis.set_ticks([])
+        axs[seq//2,seq%2].axes.yaxis.set_ticks([])
+        axs[seq//2,seq%2].set_title(meanings[colour])
 
     fig.suptitle(f'{image_id}: {"+".join([Descriptions[label] for label in Training[image_id]])}')
-    savefig(join(figs,image_id))
+    savefig(join(figs,image_id), dpi=dpi)
     return fig
+
+# restrict
+#
+# Used to restrict traing data to specified labels
+
+def restrict(Training,labels):
+    return {image_id: image_labels for image_id,image_labels in Training.items() if len(set(image_labels)& set(labels))>0}
+
+
 
 if __name__=='__main__':
     parser = ArgumentParser('Visualize HPA data')
-    parser.add_argument('--path',      default = join(environ['DATA'],'hpa-scc'))
-    parser.add_argument('--image_set', default = 'train512x512')
-    parser.add_argument('--image_id',  default = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0')
-    parser.add_argument('--figs',      default = './figs',  help    = 'Identifies where to store plots')
-    parser.add_argument('--sample',    default = None, type=int)
-    parser.add_argument('--show',      default = False, action='store_true')
-    parser.add_argument('--multiple',  default = False, action='store_true')
-    parser.add_argument('--all' ,      default = False, action='store_true')
+    parser.add_argument('--path',
+                        default = join(environ['DATA'],'hpa-scc'),
+                        help    = 'Folder for data files')
+    parser.add_argument('--image_set',
+                        default = 'train512x512',
+                        help    = 'Set resolution of raw images')
+    parser.add_argument('--image_id',
+                        default = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0',
+                        help    = 'Used to vew a single image only')
+    parser.add_argument('--figs',
+                        default = './figs',
+                        help    = 'Identifies where to store plots')
+    parser.add_argument('--sample',
+                        default = None,
+                        type    = int,
+                        help    = 'Used to sample a specified number of images')
+    parser.add_argument('--show',
+                        default = False,
+                        action  = 'store_true',
+                        help    = 'Display images')
+    parser.add_argument('--multiple',
+                        default = False,
+                        action  = 'store_true',
+                        help    = 'Process images that belong to multiple labels')
+    parser.add_argument('--all' ,
+                        default = False,
+                        action  = 'store_true',
+                        help    = 'Process all images, subject to filtering on labels')
+    parser.add_argument('--labels',
+                        default = list(range(19)),
+                        type    = int,
+                        nargs   = '*',
+                        help    = 'Used to restrict Locations')
+    parser.add_argument('--seed',
+                        default = None,
+                        type    = int,
+                        help    = 'Used to seed random number generator')
+    parser.add_argument('--dpi',
+                        default = 300,
+                        type    = int,
+                        help    = 'Resolution for saving images')
     args         = parser.parse_args()
-
+    seed(args.seed)
     Descriptions = read_descriptions('descriptions.csv')
-    Training     = read_training_expectations(path=args.path)
+    Training     = restrict(read_training_expectations(path=args.path),
+                            labels = args.labels)
 
     if args.all:
         for image_id, cell_types in Training.items():
