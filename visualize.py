ac968e653abdcf705aa26323fe6af701c502f29c
Simon Crase <simon@greenweaves.nz>
2021-03-20 12:49:41 +1300

    #34 Show overjlap between green and each of other 3 channels

--- visualize.py
+++ visualize.py
@@ -41,7 +41,7 @@ colours     = ['red',
 meanings    = ['Microtubules',
                'Protein/antibody',
                'Nuclei channels',
-               'Endoplasmic reticulum channels'
+               'Endoplasmic reticulum'
               ]
 
 # read_descriptions
@@ -69,37 +69,52 @@ def visualize(image_id     = '5c27f04c-bb99-11e8-b2b9-ac1f6b6435d0',
               figs         = './figs',
               Descriptions = [],
               dpi          = 300):
-    fig          = figure(figsize=(20,20))
-    axs          = fig.subplots(2, 2)
+    fig          = figure(figsize=(20,10))
+    axs          = fig.subplots(2, 4)
+    Greys        = []
 
     for seq,colour in enumerate([BLUE,RED,YELLOW,GREEN]):
         file_name        = f'{image_id}_{colours[colour]}.png'
         path_name        = join(path,image_set,file_name)
-        grey_scale_image = imread(path_name)
-        nx,ny            = grey_scale_image.shape
+        Greys.append(imread(path_name))
+        nx,ny            = Greys[-1].shape
         Image            = zeros((nx,ny,NCOLOURS))
 
         if colour==YELLOW:
-            Image[:,:,RED]    = grey_scale_image[:,:]
-            Image[:,:,GREEN]  = grey_scale_image[:,:]
+            Image[:,:,RED]    = Greys[-1][:,:]
+            Image[:,:,GREEN]  = Greys[-1][:,:]
         else:
-            Image[:,:,colour] = grey_scale_image[:,:]
+            Image[:,:,colour] = Greys[-1][:,:]
 
-        axs[seq//2,seq%2].imshow(Image)
-        axs[seq//2,seq%2].axes.xaxis.set_ticks([])
-        axs[seq//2,seq%2].axes.yaxis.set_ticks([])
-        axs[seq//2,seq%2].set_title(meanings[colour])
+        axs[0,seq].imshow(Image)
+        axs[0,seq].axes.xaxis.set_ticks([])
+        axs[0,seq].axes.yaxis.set_ticks([])
+        axs[0,seq].set_title(meanings[colour])
 
+    for seq,colour in enumerate([BLUE,RED,YELLOW]):
+        nx,ny           = Greys[-1].shape
+        Image           = zeros((nx,ny,NCOLOURS))
+        Image[:,:,BLUE] = Greys[seq][:,:]
+        Image[:,:,RED]  = Greys[-1][:,:]
+        axs[1,seq].imshow(Image)
+        axs[1,seq].axes.xaxis.set_ticks([])
+        axs[1,seq].axes.yaxis.set_ticks([])
+        axs[1,seq].set_title(f'{meanings[GREEN]}+{meanings[colour]}')
+
+    axs[1,NCOLOURS].axes.xaxis.set_ticks([])
+    axs[1,NCOLOURS].axes.yaxis.set_ticks([])
     fig.suptitle(f'{image_id}: {"+".join([Descriptions[label] for label in Training[image_id]])}')
-    savefig(join(figs,image_id), dpi=dpi)
+    savefig(join(figs,image_id), dpi=dpi, bbox_inches='tight')
     return fig
 
 # restrict
 #
-# Used to restrict traing data to specified labels
+# Used to restrict training data to specified labels
 
-def restrict(Training,labels):
-    return {image_id: image_labels for image_id,image_labels in Training.items() if len(set(image_labels)& set(labels))>0}
+def restrict(Training,labels,multiple=False):
+    def should_include(image_labels):
+        return (len(set(image_labels)& set(labels))>0) and (multiple or len(image_labels)==1)
+    return {image_id: image_labels for image_id,image_labels in Training.items() if should_include(image_labels)}
 
 
 
@@ -150,7 +165,8 @@ if __name__=='__main__':
     seed(args.seed)
     Descriptions = read_descriptions('descriptions.csv')
     Training     = restrict(read_training_expectations(path=args.path),
-                            labels = args.labels)
+                            labels   = args.labels,
+                            multiple = args.multiple)
 
     if args.all:
         for image_id, cell_types in Training.items():
@@ -170,14 +186,13 @@ if __name__=='__main__':
                   Descriptions = Descriptions)
     else:
         for image_id in sample(list(Training.keys()),args.sample):
-            if args.multiple or len(Training[image_id]) == 1:
-                fig = visualize(image_id     = image_id,
-                                path         = args.path,
-                                image_set    = args.image_set,
-                                figs         = args.figs,
-                                Descriptions = Descriptions)
-                if not args.show:
-                    close(fig)
+            fig = visualize(image_id     = image_id,
+                            path         = args.path,
+                            image_set    = args.image_set,
+                            figs         = args.figs,
+                            Descriptions = Descriptions)
+            if not args.show:
+                close(fig)
 
     if args.show:
         show()
