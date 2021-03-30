be3b14a6ceb9f1ec4802dada277d2fe6a32908e1
Simon Crase <simon@greenweaves.nz>
2021-03-31 09:38:38 +1300

    Use Timer from utils.py

--- analyze-labels.py
+++ analyze-labels.py
@@ -22,52 +22,47 @@ from   argparse          import ArgumentParser
 from   csv               import writer
 from   random            import shuffle
 from   segment           import read_training_expectations,create_descriptions
-from   time              import time
+from   utils             import Timer
 
 if __name__=='__main__':
-    start  = time()
-    parser = ArgumentParser('Find images  that have only one label')
-    parser.add_argument('--path',
-                        default = r'd:\data\hpa-scc',
-                        help    = 'Path to data')
-    parser.add_argument('--out',
-                        default = 'singletons.csv',
-                        help    = 'Output file')
-    parser.add_argument('--selection',
-                        default = 'selection.txt',
-                        help    = 'Selection of files to be processed by segment.py')
-    parser.add_argument('--classes',
-                        default = ['all'],
-                        nargs   = '+',
-                        help    = 'List of classes to be processed by segment.py')
-    parser.add_argument('--n',
-                        default = 1,
-                        type    = int,
-                        help    = 'Number of instances of each class to be processed by segment.py')
-    args     = parser.parse_args()
-    descriptions = create_descriptions()
-    with open(args.out,'w',newline='') as csvfile:
-        singleton_writer = writer(csvfile)
-        for image_id,label in sorted([(image_id,labels[0])
-             for image_id,labels in read_training_expectations(path=args.path).items()
-                if len(labels)==1],key=lambda s:s[1]):
-            singleton_writer.writerow([label,image_id])
+    with Timer():
+        parser = ArgumentParser('Find images  that have only one label')
+        parser.add_argument('--path',
+                            default = r'd:\data\hpa-scc',
+                            help    = 'Path to data')
+        parser.add_argument('--out',
+                            default = 'singletons.csv',
+                            help    = 'Output file')
+        parser.add_argument('--selection',
+                            default = 'selection.txt',
+                            help    = 'Selection of files to be processed by segment.py')
+        parser.add_argument('--classes',
+                            default = ['all'],
+                            nargs   = '+',
+                            help    = 'List of classes to be processed by segment.py')
+        parser.add_argument('--n',
+                            default = 1,
+                            type    = int,
+                            help    = 'Number of instances of each class to be processed by segment.py')
+        args     = parser.parse_args()
+        descriptions = create_descriptions()
+        with open(args.out,'w',newline='') as csvfile:
+            singleton_writer = writer(csvfile)
+            for image_id,label in sorted([(image_id,labels[0])
+                 for image_id,labels in read_training_expectations(path=args.path).items()
+                    if len(labels)==1],key=lambda s:s[1]):
+                singleton_writer.writerow([label,image_id])
 
-    if len(args.classes)>0:
-        classes = list(range(len(descriptions)+1)) if args.classes[0]=='all' else args.classes
-        counts  = [0 for _ in classes]
-        with open(args.selection,'w') as out:
-            singletons   = {image_id:label[0] for image_id,label in read_training_expectations(path=args.path).items() if len(label)==1}
-            file_names   = list(singletons.keys())
-            shuffle(file_names)
-            for i in range(len(file_names)):
-                if min(counts)>=args.n: break
-                j = singletons[file_names[i]]
-                if j < len(counts) and counts[j]<args.n:
-                    out.write(f'{file_names[i]}\n')
-                    counts[j] += 1
-
-    elapsed = time() - start
-    minutes = int(elapsed/60)
-    seconds = elapsed - 60*minutes
-    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
+        if len(args.classes)>0:
+            classes = list(range(len(descriptions)+1)) if args.classes[0]=='all' else args.classes
+            counts  = [0 for _ in classes]
+            with open(args.selection,'w') as out:
+                singletons   = {image_id:label[0] for image_id,label in read_training_expectations(path=args.path).items() if len(label)==1}
+                file_names   = list(singletons.keys())
+                shuffle(file_names)
+                for i in range(len(file_names)):
+                    if min(counts)>=args.n: break
+                    j = singletons[file_names[i]]
+                    if j < len(counts) and counts[j]<args.n:
+                        out.write(f'{file_names[i]}\n')
+                        counts[j] += 1
